dfbefee0cd6f719799aca9db4e1264f9a686c04a
Wisbell <westleyisbell@gmail.com>
2017-05-03 14:34:16 -0500

    Testing mobile

---
+++ new_index.js
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Westley Isbell - Web Developer Portfolio</title>
+  <!-- Latest compiled and minified CSS -->
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
+
+  <!-- Ensures proper rending and touch zooming -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+  <div class="container-fluid">
+
+
+    <!-- Main Section -->
+    <div class="row">
+
+      <div class="col-md-12">
+          <h1>Westley Isbell</h1>
+          <h3>Junior Web Developer</h3>
+          <h3>NSS - Cohort 17</h3>
+      </div>
+
+    </div>
+
+    <div class="row">
+
+      <div class="col-md-12">
+        <ul>
+          <li col-md-3><a href="#">Home</a></li>
+          <li col-md-3> <a href="#">About</a></li>
+          <li col-md-3> <a href="#">Projects</a></li>
+          <li col-md-3> <a href="#">Contact</a></li>
+        </ul>
+      </div>
+
+    </div>
+
+
+
+  </div> <!-- Close container-fluid wrapper -->
+
+<script
+  src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</body>
+</html>
